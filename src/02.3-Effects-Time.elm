d1d28ca7b5d0f9ae0022fdbdcf45d92b1ed19354
Anton Strömkvist <hi@ahst.ro>
2017-02-03 08:42:49 +0100

    Part 2.3.2: Effects - Time - Add weird pause button
    When clicking "Play" again, it starts at the actual time, not where it
    was paused, which makes sense, so I'm not going to try to hack it. If I
    wanted a stopwatch, I'd create a counter that incremented every second
    instead of using the actual time.

--- src/02.3-Effects-Time.elm
+++ src/02.3-Effects-Time.elm
@@ -1,6 +1,7 @@
 module TimeStuff exposing (..)
 
-import Html exposing (Html)
+import Html exposing (..)
+import Html.Events exposing (onClick)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
@@ -16,35 +17,44 @@ main =
 
 
 type alias Model =
-    Time
+    { time : Time
+    , paused : Bool
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( 0, Cmd.none )
+    ( Model 0 False, Cmd.none )
 
 
 type Msg
     = Tick Time
+    | Pause
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Tick newTime ->
-            ( newTime, Cmd.none )
+            ( { model | time = newTime }, Cmd.none )
+
+        Pause ->
+            ( { model | paused = not model.paused }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Time.every second Tick
+    if model.paused then
+        Sub.none
+    else
+        Time.every second Tick
 
 
 view : Model -> Html Msg
 view model =
     let
         angle =
-            turns (Time.inMinutes model)
+            turns (Time.inMinutes model.time)
 
         handX =
             toString (50 + 40 * cos angle)
@@ -52,7 +62,20 @@ view model =
         handY =
             toString (50 + 40 * sin angle)
     in
-        svg [ viewBox "0 0 100 100", width "300px" ]
-            [ circle [ cx "50", cy "50", r "45", fill "#0b79ce" ] []
-            , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+        div []
+            [ svg [ viewBox "0 0 100 100", width "300px" ]
+                [ circle [ cx "50", cy "50", r "45", fill "#0b79ce" ] []
+                , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+                ]
+            , button [ onClick Pause ]
+                [ Html.text (getButtonText model.paused)
+                ]
             ]
+
+
+getButtonText : Bool -> String
+getButtonText paused =
+    if paused then
+        "Play"
+    else
+        "Pause"
