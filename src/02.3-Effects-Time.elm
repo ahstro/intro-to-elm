10039c984d390b1db390136d294a961552e9bba5
Anton Strömkvist <hi@ahst.ro>
2017-02-08 00:09:34 +0100

    Part 2.3.3: Effects - Time - Add hour and minute hands
    Also, rewrite everything and understand what's going on

--- src/02.3-Effects-Time.elm
+++ src/02.3-Effects-Time.elm
@@ -53,24 +53,67 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     let
-        angle =
-            turns (Time.inMinutes model.time)
+        time =
+            Time.inSeconds model.time
 
-        handX =
-            toString (50 + 40 * cos angle)
+        seconds =
+            (floor time) % 60
 
-        handY =
-            toString (50 + 40 * sin angle)
+        minutes =
+            (floor (time / 60)) % 60
+
+        hours =
+            (ceiling (time / 60 / 60)) % 12
+
+        sAngle =
+            degrees (toFloat (seconds * 6) - 90)
+
+        mAngle =
+            degrees (toFloat (minutes * 6) - 90)
+
+        hAngle =
+            degrees (toFloat (hours * 30) - 90)
+
+        sHandX =
+            toString (getXFromAngle sAngle)
+
+        sHandY =
+            toString (getYFromAngle sAngle)
+
+        mHandX =
+            toString (getXFromAngle mAngle)
+
+        mHandY =
+            toString (getYFromAngle mAngle)
+
+        hHandX =
+            toString (getXFromAngle hAngle)
+
+        hHandY =
+            toString (getYFromAngle hAngle)
     in
         div []
             [ svg [ viewBox "0 0 100 100", width "300px" ]
                 [ circle [ cx "50", cy "50", r "45", fill "#0b79ce" ] []
-                , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+                , line [ x1 "50", y1 "50", x2 sHandX, y2 sHandY, stroke "#ff0000" ] []
+                , line [ x1 "50", y1 "50", x2 mHandX, y2 mHandY, stroke "#00ff00" ] []
+                , line [ x1 "50", y1 "50", x2 hHandX, y2 hHandY, stroke "#0000ff" ] []
                 ]
+            , div [] [ Html.text ((toString hours) ++ ":" ++ (toString minutes) ++ ":" ++ (toString seconds)) ]
             , button [ onClick Pause ]
                 [ Html.text (getButtonText model.paused)
                 ]
             ]
+
+
+getXFromAngle : Float -> Float
+getXFromAngle angle =
+    50 + 40 * cos angle
+
+
+getYFromAngle : Float -> Float
+getYFromAngle angle =
+    50 + 40 * sin angle
 
 
 getButtonText : Bool -> String
